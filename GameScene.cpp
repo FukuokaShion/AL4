9c794c557f24fa7f50f22ba48993e16ba4b35515
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-01-07 19:07:08 +0900

    確認課題03

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,40 +1,53 @@
 ﻿#include "GameScene.h"
+#include "Collision.h"
+#include <sstream>
+#include <iomanip>
 #include <cassert>
 
 using namespace DirectX;
-
 GameScene::GameScene()
 {
 }
-
 GameScene::~GameScene()
 {
 	delete spriteBG;
 	delete object3d;
 }
-
 void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
 {
 	// nullptrチェック
 	assert(dxCommon);
 	assert(input);
-
 	this->dxCommon = dxCommon;
 	this->input = input;
-
 	// デバッグテキスト用テクスチャ読み込み
 	Sprite::LoadTexture(debugTextTexNumber, L"Resources/debugfont.png");
 	// デバッグテキスト初期化
 	debugText.Initialize(debugTextTexNumber);
-
 	// テクスチャ読み込み
 	Sprite::LoadTexture(1, L"Resources/background.png");
-
 	// 背景スプライト生成
 	spriteBG = Sprite::Create(1, { 0.0f,0.0f });
 	// 3Dオブジェクト生成
 	object3d = Object3d::Create();
 	object3d->Update();
+
+
+	//弾の初期化
+	sphere.center = XMVectorSet(0, 2, 0, 1);//中心
+	//半径
+	sphere.radius = 1.0f;
+
+	//平面の初期化
+	plane.normal = XMVectorSet(0, 1, 0, 0);//法線ベクトル
+	plane.distance = 0.0f;//原点からの距離
+
+	//三角形の初期値を設定
+	triangle.p0 = XMVectorSet(-1.0f, 0, -1.0f, 1);
+	triangle.p1 = XMVectorSet(-1.0f, 0, +1.0f, 1);
+	triangle.p2 = XMVectorSet(+1.0f, 0, -1.0f, 1);
+	triangle.normal = XMVectorSet(0.0f, 1.0, 0.0f, 0);
+
 }
 
 void GameScene::Update()
@@ -44,17 +57,14 @@ void GameScene::Update()
 	{
 		// 現在の座標を取得
 		XMFLOAT3 position = object3d->GetPosition();
-
 		// 移動後の座標を計算
 		if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
 		else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
 		if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
 		else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
-
 		// 座標の変更を反映
 		object3d->SetPosition(position);
 	}
-
 	// カメラ移動
 	if (input->PushKey(DIK_W) || input->PushKey(DIK_S) || input->PushKey(DIK_D) || input->PushKey(DIK_A))
 	{
@@ -64,6 +74,72 @@ void GameScene::Update()
 		else if (input->PushKey(DIK_A)) { Object3d::CameraMoveVector({ -1.0f,0.0f,0.0f }); }
 	}
 
+
+	//球移動
+	{
+		//Y軸方向移動
+		XMVECTOR moveY = XMVectorSet(0, 0.01f, 0, 0);
+		if (input->PushKey(DIK_1))
+		{
+			sphere.center += moveY;
+		}
+		else if (input->PushKey(DIK_2))
+		{
+			sphere.center -= moveY;
+		}
+		//X軸方向移動
+		XMVECTOR moveX = XMVectorSet(0.01f, 0, 0, 0);
+		if (input->PushKey(DIK_3))
+		{
+			sphere.center += moveX;
+		}
+		else if (input->PushKey(DIK_4))
+		{
+			sphere.center -= moveX;
+		}
+	}
+	//stringstreamで変数の値を埋め込んで整形する
+	std::ostringstream sqherestr;
+	sqherestr << "Sqhere:("
+		<< std::fixed << std::setprecision(2)//少数第２まで
+		<< sphere.center.m128_f32[0] << ","//x
+		<< sphere.center.m128_f32[1] << ","//y
+		<< sphere.center.m128_f32[2] << ")";//z
+	debugText.Print(sqherestr.str(), 50, 180, 1.0f);
+
+	//当たり判定
+	//bool hit = Collision::CheckSphere2Plane(sphere, plane);
+	//if (hit)
+	//{
+	//	debugText.Print("HIT", 50, 200, 1.0f);
+	//	//stringstreamリセット,交点座標を埋め込む
+	//	sqherestr.str("");
+	//	sqherestr.clear();
+	//	sqherestr << "("
+	//		<< std::fixed << std::setprecision(2)//少数第２まで
+	//		<< sphere.center.m128_f32[0] << ","//x
+	//		<< sphere.center.m128_f32[1] << ","//y
+	//		<< sphere.center.m128_f32[2] << ")";//z
+	//	debugText.Print(sqherestr.str(), 50, 220, 1.0f);
+	//}
+
+
+
+	XMVECTOR inter;
+	bool hit = Collision::CheckSphere2Triangle(sphere, triangle, &inter);
+	if (hit)
+	{
+		debugText.Print("HIT", 50, 200, 1.0f);
+		sqherestr.str("");
+		sqherestr.clear();
+		sqherestr << "("
+			<< std::fixed << std::setprecision(2)//少数第２まで
+			<< sphere.center.m128_f32[0] << ","//x
+			<< sphere.center.m128_f32[1] << ","//y
+			<< sphere.center.m128_f32[2] << ")";//z
+		debugText.Print(sqherestr.str(), 50, 220, 1.0f);
+	}
+
 	object3d->Update();
 }
 
@@ -71,49 +147,38 @@ void GameScene::Draw()
 {
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
-
 #pragma region 背景スプライト描画
 	// 背景スプライト描画前処理
 	Sprite::PreDraw(cmdList);
 	// 背景スプライト描画
-	//spriteBG->Draw();
-
+	spriteBG->Draw();
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
 	dxCommon->ClearDepthBuffer();
 #pragma endregion
-
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
 	Object3d::PreDraw(cmdList);
-
 	// 3Dオブクジェクトの描画
 	object3d->Draw();
-
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
 	// 3Dオブジェクト描画後処理
 	Object3d::PostDraw();
 #pragma endregion
-
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(cmdList);
-
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
 	// デバッグテキストの描画
 	debugText.DrawAll(cmdList);
-
 	// スプライト描画後処理
 	Sprite::PostDraw();
 #pragma endregion
--- GameScene.h
+++ GameScene.h
@@ -1,11 +1,11 @@
 ﻿#pragma once
-
 #include "DirectXCommon.h"
 #include <DirectXMath.h>
 #include "Input.h"
 #include "Sprite.h"
 #include "Object3d.h"
 #include "DebugText.h"
+#include "CollisionPrimitive.h"
 
 /// <summary>
 /// ゲームシーン
@@ -20,47 +20,42 @@ private: // エイリアス
 	using XMFLOAT3 = DirectX::XMFLOAT3;
 	using XMFLOAT4 = DirectX::XMFLOAT4;
 	using XMMATRIX = DirectX::XMMATRIX;
-
 private: // 静的メンバ変数
 	static const int debugTextTexNumber = 0;
-
 public: // メンバ関数
-
 	/// <summary>
 	/// コンストクラタ
 	/// </summary>
 	GameScene();
-
 	/// <summary>
 	/// デストラクタ
 	/// </summary>
 	~GameScene();
-
 	/// <summary>
 	/// 初期化
 	/// </summary>
 	void Initialize(DirectXCommon* dxCommon, Input* input);
-
 	/// <summary>
 	/// 毎フレーム処理
 	/// </summary>
 	void Update();
-
 	/// <summary>
 	/// 描画
 	/// </summary>
 	void Draw();
-
 private: // メンバ変数
 	DirectXCommon* dxCommon = nullptr;
 	Input* input = nullptr;
-	DebugText debugText;	
-
+	DebugText debugText;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
 	Sprite* spriteBG = nullptr;
 	Object3d* object3d = nullptr;
 
-};
+	Sphere sphere;
 
+	Plane plane;
+
+	Triangle triangle;
+};
